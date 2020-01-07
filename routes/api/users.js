fb88f633419e6e74192788c4aa2c99ef19f5b432
hamid <hamid.nikoo@gmail.com>
2020-01-07 13:45:18 +0100

    message

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
 // @route  POST api/users
@@ -27,7 +29,7 @@ async (req, res)=> {
     try {
      let user = await User.findOne({ email: email});
      if(user){
-         res.status(400).json({ errors: [{ msg: 'User already exists'}]});
+         return res.status(400).json({ errors: [{ msg: 'User already exists'}]});
      }
      // Get users gravatar
     const avatar = gravatar.url(email, {
@@ -48,10 +50,22 @@ async (req, res)=> {
     await user.save();
 
     // Return jsonwebtoken
+    const payload = {
+        user: {
+         id: user.id
+        }
+    }
+    
+    jwt.sign(
+        payload, 
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+         if(err) throw err;
+         res.json({ token });
+        });
 
-    res.send('User registered');
-
-    } catch(err){
+    } catch(err) {
       console.error(err.message);
       res.status(500).send('Server error')
     }
