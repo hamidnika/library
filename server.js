b318c7e60374782146c46fa28f16f095e91541d5
hamid <hamid.nikoo@gmail.com>
2020-04-11 15:39:52 +0200

    11april

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.use(express.json());
 
-/* // Serve up static assets (usually on heroku)
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "./client/build")));
-} */
+}
 
 app.get("/", (req, res) => res.send("API running"));
 
