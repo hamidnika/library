b5da16b2be54b54edd55e285b7a3df16ba63c5b5
hamid <hamid.nikoo@gmail.com>
2020-04-11 14:49:57 +0200

    11april

--- server.js
+++ server.js
@@ -28,11 +28,22 @@ app.use("/api/profile", require("./routes/api/profile"));
 
   
 var cors = require('cors');
-app.use(cors({origin: "*",
-methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-preflightContinue: false,
-optionsSuccessStatus: 204}));
-
+//app.use(cors());
+var allowedOrigins = ['http://localhost:3000',
+                      'http://mernudem.herokuapp.com/bk'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 // use Routes
 app.use('/api/bookss', require('./routes/api/books'));
 
