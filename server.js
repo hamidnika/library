7f6c359a9f5897c8169e8e99634af164b1e0cde2
hamid <hamid.nikoo@gmail.com>
2020-04-11 17:21:06 +0200

    11april

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const app = express();
 connectDB();
 
 // Init Middleware
-app.use(express.json({ extended: false }));
-app.use(express.json());
-
+app.use(express.json({ extended: true }));
+/* app.use(express.json());
+ */
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "./client/build")));
