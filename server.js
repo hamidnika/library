e444a0ee9f80113cab08c7791e754ac41fdb50d3
hamid <hamid.nikoo@gmail.com>
2020-04-13 17:48:43 +0200

    13april

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const connectDB = require("./config/db");
 const apiRoutes = require("./routes/api-routes");
 const path = require("path");
 const app = express();
-const mongoose = require("mongoose");
+
 
 // express Connect Database
 connectDB();
 
 // Init Middleware
-app.use(express.urlencoded({ extended: true}))
+//app.use(express.urlencoded({ extended: true}))
 app.use(express.json({ extended: true }));
 
 
@@ -41,7 +41,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-mongoose.connect("mongodb+srv://http://mernudem.herokuapp.com")
 
 const PORT = process.env.PORT || 5000;
 
