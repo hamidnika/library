6137b83078d6c3e91c34ead934f8257307eedfbe
hamid <hamid.nikoo@gmail.com>
2020-04-18 12:39:14 +0200

    18april

--- server.js
+++ server.js
@@ -52,7 +52,12 @@ app.use(cors({ origin: true, credentials: true }));
 
 // use Routes
 app.use('/api/bookss', require('./routes/api/books'));
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET, POST");
+  next();
+});
 
 // Define API routes here
 app.use("/api", apiRoutes);
