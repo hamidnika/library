8c48b9a329537e6b80a7aa6a44ab4392d3f130a6
hamid <hamid.nikoo@gmail.com>
2020-04-19 21:58:24 +0200

    18april

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "./client/build")));
 }
 
-app.get("/", (req, res) => res.send("API running"));
+//app.get("/", (req, res) => res.send("API running"));
 var cors = require('cors');
 app.use(cors({ origin: true, credentials: true }));
 // define routes
