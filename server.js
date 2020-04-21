dddfd43fe62861b728a34618150c206cc04643c1
hamid <hamid.nikoo@gmail.com>
2020-04-22 00:59:48 +0200

    21april

--- server.js
+++ server.js
@@ -27,10 +27,6 @@ app.post('/upload', (req, res) => {
 // express Connect Database
 connectDB();
 
-// Init Middleware
-//app.use(express.urlencoded({ extended: true}))
-app.use(express.json({ extended: true }));
-
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -40,20 +36,17 @@ if (process.env.NODE_ENV === "production") {
 app.get("/", (req, res) => res.send("API running"));
 var cors = require('cors');
 app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 // define routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
-
-
-
-
-// use Routes
 app.use('/api/bookss', require('./routes/api/books'));
-
-
-// Define API routes here
 app.use("/api", apiRoutes);
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
